d86c0526c3a557a0988ee9965c59b402f6f42e4c
Parker Smith <parker@parkersmith.us>
2018-04-08 23:24:30 -0600

    Implement helper methods

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -2,4 +2,21 @@
  * Created by parker on 4/8/18.
  */
 public class SelectionSort {
+
+    public static void sort(Comparable[] arr)
+    {
+
+    }
+
+    private static boolean less(Comparable v, Comparable w)
+    {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exchange(Comparable[] arr, int i, int j)
+    {
+        Comparable swap = arr[i];
+        arr[i] = arr[j];
+        arr[j] = swap;
+    }
 }
