b4a661393a185bfa907bd26f7176f5882c918539
Parker Smith <parker@parkersmith.us>
2018-04-08 23:09:06 -0600

    Add test for selection sort

--- test/SelectionSortTest.java
+++ test/SelectionSortTest.java
@@ -1,3 +1,6 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -5,4 +8,12 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class SelectionSortTest {
 
+    @Test
+    void test_sort_works()
+    {
+        int[] unsorted = {7, 9, 8, 2, 1, 3, 5, 4, 6};
+        int[] sorted = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        Assertions.assertEquals(sorted, SelectionSort.sort(unsorted));
+    }
 }
