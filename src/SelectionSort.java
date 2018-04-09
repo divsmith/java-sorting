91d7a3e98b10c9c8def11e55fd19c0af8adbc560
Parker Smith <parker@parkersmith.us>
2018-04-08 23:33:10 -0600

    Implement selection sort

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -5,7 +5,19 @@ public class SelectionSort {
 
     public static void sort(Comparable[] arr)
     {
+        for (int i = 0; i < arr.length; i++)
+        {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++)
+            {
+                if (less(arr[j], arr[min]))
+                {
+                    min = j;
+                }
+            }
 
+            exchange(arr, i, min);
+        }
     }
 
     private static boolean less(Comparable v, Comparable w)
