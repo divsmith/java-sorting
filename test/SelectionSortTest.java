8f879c28bf40a2eddd424c239841df086b946651
Parker Smith <parker@parkersmith.us>
2018-04-08 23:24:38 -0600

    Fix test to use Integer objects

--- test/SelectionSortTest.java
+++ test/SelectionSortTest.java
@@ -11,9 +11,11 @@ class SelectionSortTest {
     @Test
     void test_sort_works()
     {
-        int[] unsorted = {7, 9, 8, 2, 1, 3, 5, 4, 6};
-        int[] sorted = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Integer[] arr = {7, 9, 8, 2, 1, 3, 5, 4, 6};
+        Integer[] sorted = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-        Assertions.assertEquals(sorted, SelectionSort.sort(unsorted));
+        SelectionSort.sort(arr);
+
+        Assertions.assertEquals(sorted, arr);
     }
 }
